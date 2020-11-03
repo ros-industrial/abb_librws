2c7f3951d5928df229e2b6c2235e930914205a6d
jontje <jon.tjerngren@se.abb.com>
2020-11-03 10:42:51 +0100

    Added error handling

--- src/rws_interface.cpp
+++ src/rws_interface.cpp
@@ -230,6 +230,7 @@ std::vector<cfg::moc::Single> RWSInterface::getCFGSingles()
   std::vector<cfg::moc::Single> result;
 
   RWSClient::RWSResult rws_result = rws_client_.getConfigurationInstances(Identifiers::MOC, Identifiers::SINGLE);
+  if(!rws_result.success) throw std::runtime_error(EXCEPTION_GET_CFG);
 
   std::vector<Poco::XML::Node*> instances;
   instances = xmlFindNodes(rws_result.p_xml_document, XMLAttributes::CLASS_CFG_DT_INSTANCE_LI);
@@ -246,56 +247,67 @@ std::vector<cfg::moc::Single> RWSInterface::getCFGSingles()
       if(xmlNodeHasAttribute(attribute, Identifiers::TITLE, Identifiers::NAME))
       {
         single.name = xmlFindTextContent(attribute, XMLAttributes::CLASS_VALUE);
+        if(single.name.empty()) throw std::runtime_error(EXCEPTION_PARSE_CFG);
       }
       else if(xmlNodeHasAttribute(attribute, Identifiers::TITLE, "use_single_type"))
       {
         single.use_single_type = xmlFindTextContent(attribute, XMLAttributes::CLASS_VALUE);
+        if(single.use_single_type.empty()) throw std::runtime_error(EXCEPTION_PARSE_CFG);
       }
       else if(xmlNodeHasAttribute(attribute, Identifiers::TITLE, "use_joint"))
       {
         single.use_joint = xmlFindTextContent(attribute, XMLAttributes::CLASS_VALUE);
+        if(single.use_joint.empty()) throw std::runtime_error(EXCEPTION_PARSE_CFG);
       }
       else if(xmlNodeHasAttribute(attribute, Identifiers::TITLE, "base_frame_pos_x"))
       {
         std::stringstream ss(xmlFindTextContent(attribute, XMLAttributes::CLASS_VALUE));
         ss >> single.base_frame.pos.x.value;
+        if(ss.fail()) throw std::runtime_error(EXCEPTION_PARSE_CFG);
         single.base_frame.pos.x.value *= 1000.0;
       }
       else if(xmlNodeHasAttribute(attribute, Identifiers::TITLE, "base_frame_pos_y"))
       {
         std::stringstream ss(xmlFindTextContent(attribute, XMLAttributes::CLASS_VALUE));
         ss >> single.base_frame.pos.y.value;
+        if(ss.fail()) throw std::runtime_error(EXCEPTION_PARSE_CFG);
         single.base_frame.pos.y.value *= 1000.0;
       }
       else if(xmlNodeHasAttribute(attribute, Identifiers::TITLE, "base_frame_pos_z"))
       {
         std::stringstream ss(xmlFindTextContent(attribute, XMLAttributes::CLASS_VALUE));
         ss >> single.base_frame.pos.z.value;
+        if(ss.fail()) throw std::runtime_error(EXCEPTION_PARSE_CFG);
         single.base_frame.pos.z.value *= 1000.0;
       }
       else if(xmlNodeHasAttribute(attribute, Identifiers::TITLE, "base_frame_orient_u0"))
       {
         std::stringstream ss(xmlFindTextContent(attribute, XMLAttributes::CLASS_VALUE));
         ss >> single.base_frame.rot.q1.value;
+        if(ss.fail()) throw std::runtime_error(EXCEPTION_PARSE_CFG);
       }
       else if(xmlNodeHasAttribute(attribute, Identifiers::TITLE, "base_frame_orient_u1"))
       {
         std::stringstream ss(xmlFindTextContent(attribute, XMLAttributes::CLASS_VALUE));
         ss >> single.base_frame.rot.q2.value;
+        if(ss.fail()) throw std::runtime_error(EXCEPTION_PARSE_CFG);
       }
       else if(xmlNodeHasAttribute(attribute, Identifiers::TITLE, "base_frame_orient_u2"))
       {
         std::stringstream ss(xmlFindTextContent(attribute, XMLAttributes::CLASS_VALUE));
         ss >> single.base_frame.rot.q3.value;
+        if(ss.fail()) throw std::runtime_error(EXCEPTION_PARSE_CFG);
       }
       else if(xmlNodeHasAttribute(attribute, Identifiers::TITLE, "base_frame_orient_u3"))
       {
         std::stringstream ss(xmlFindTextContent(attribute, XMLAttributes::CLASS_VALUE));
         ss >> single.base_frame.rot.q4.value;
+        if(ss.fail()) throw std::runtime_error(EXCEPTION_PARSE_CFG);
       }
       else if(xmlNodeHasAttribute(attribute, Identifiers::TITLE, "base_frame_coordinated"))
       {
         single.base_frame_coordinated = xmlFindTextContent(attribute, XMLAttributes::CLASS_VALUE);
+        if(single.base_frame_coordinated.empty()) throw std::runtime_error(EXCEPTION_PARSE_CFG);
       }
     }
 
