5640f13b7ed354277014bc445a57e70169460106
Krzysztof Gomulski <kgomulski@nomagic.ai>
2023-04-27 23:01:22 +0200

    GRASP-13822 type fix

--- include/abb_librws/common/rw/ctrl.h
+++ include/abb_librws/common/rw/ctrl.h
@@ -27,13 +27,13 @@ namespace abb :: rws :: rw
     struct SafetyViolationInfo
     {
         // Robot is unsynchronized
-        bool unsynchronized = 0;
+        bool unsynchronized = false;
         // Robot tool is outside allowed area
-        bool toolPosViolation = 0;
+        bool toolPosViolation = false;
         // Robot arm is outside allowed area
-        bool armViolation = 0;
+        bool armViolation = false;
         //One of the robot axes is outside allowed area
-        bool axisRangeViolation = 0;
+        bool axisRangeViolation = false;
     };
 
 }
